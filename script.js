a6eb86a555367b939fdb7f98b62838a5824b10e9
Leighjinn <jhunbarbosa013@gmail.com>
2026-07-01 15:07:32 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -92,3 +92,46 @@ document.querySelectorAll("#navMenu a").forEach(link => {
     });
 
 });
+
+const text = new SplitType("#portfolioText", {
+    types: "chars"
+});
+
+gsap.set("#portfolioIcon", {
+    opacity: 0,
+    scale: 0
+});
+
+const tl = gsap.timeline({
+    defaults: {
+        ease: "power3.out"
+    }
+});
+
+tl.to({}, {
+    duration: 2
+})
+
+.to("#portfolioIcon", {
+    opacity: 1,
+    scale: 1,
+    duration: 0.5,
+    ease: "back.out(2)"
+}, "-=0.2")
+
+.to(text.chars, {
+    opacity: 0,
+    x: -40,
+    y: () => gsap.utils.random(-20,20),
+    rotate: () => gsap.utils.random(-90,90),
+    scale: 0,
+    stagger: .03,
+    duration: .6
+}, "<")
+
+.to(".logo", {
+    x: -120,
+    duration: .7,
+    ease: "power2.inOut"
+}, "-=0.2");
+
